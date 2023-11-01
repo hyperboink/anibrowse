7a35758580db92bf67f28cf0bece6e92c10ba774
hyperboink <hyperboink@gmail.com>
2023-11-01 08:44:59 +0800

    remove basepath

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/anibrowse',
-  assetPrefix: '/anibrowse/',
   images: {
     domains: ['cdn.myanimelist.net']
   }
