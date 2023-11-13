754721c927eab45037a4cf50f461cd7b3213d6fd
hyperboink <hyperboink@gmail.com>
2023-11-13 21:12:15 +0800

    added title

--- components/layouts/Main.tsx
+++ components/layouts/Main.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
+import Head from 'next/head'
 import Header from './Header'
-import { PageContextProvider } from '../../context/PageContext'
+import { PageContextProvider } from '@/context/PageContext'
 import Footer from './Footer'
 
 export default function Main({children}: { children: React.ReactNode }) {
   return (
     <>
+        <Head>
+          <title>AniBrowse</title>
+        </Head>
+
         <Header/>
-        
+
         <PageContextProvider>
             {children}
         </PageContextProvider>
